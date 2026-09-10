f7fee55fd91958e74bd1ccf11fe0d95a7d2f96fb
David Sánchez <164054263+Djave17@users.noreply.github.com>
2026-09-10 15:29:34 -0600

    Añadiendo estilos

--- pokemon-app/src/components/PokemonCard.tsx
+++ pokemon-app/src/components/PokemonCard.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import type {  PokemonResponse } from "../types/pokemon";
+import "./PokemonStyle.css"
+
 
 //Props: objeto que contiene la información del Pokémon
 interface PokemonCardProps {    
@@ -18,39 +20,42 @@ export function PokemonCard({ pokemon }: PokemonCardProps) {
   
   
   return ( 
-    <article>
-      <p>#{id}</p>
-      <h2> {pokemon.name} </h2>
-      {imageUrl ? ( <img src={imageUrl} alt={pokemon.name} />) : ( <p>No hay imagen disponible</p> )}
+    <div className="bg-neutral-900 min-h-screen flex items-center justify-center p-4">
+      <article className="card">
+        <p>#{id}</p>
+        <h2> <b>{pokemon.name.toLocaleUpperCase(1)}</b> </h2>
+        {imageUrl ? ( <img className="pokemon-image" src={imageUrl} alt={pokemon.name} />) : ( <p>No hay imagen disponible</p> )}
 
-      <div className = "pokemon-dimensions"> 
-        <div>
-          <strong>{weight} kg</strong>
-          <span> Weight</span> 
-        </div>
-        <div> 
-          <strong>{height} cm</strong>
-          <span> Height</span>
+        <div className = "pokemon-dimensions"> 
+          <div>
+            <strong>{weight} kg</strong>
+            <span> Weight</span> 
+          </div>
+          <div> 
+            <strong>{height} cm</strong>
+            <span> Height</span>
+            
+          </div>
           
         </div>
-        
-      </div>
-      <div className = "pokemon-types"> 
-        {pokemon.types.map((typeItem) => (
-          <span key={typeItem.type.name} className={`pokemon-type ${typeItem.type.name}`}>
-            {typeItem.type.name}
-          </span>))}
-      </div>
-      {/*Botón para cambiar la imagen del Pokémon a Shiny */}
-      <button className = "isShiny" type = "button"  onClick={() => setIsShiny((current) => !current)}> 
-       shiny
-      </button>
+        <div className = "pokemon-types"> 
+          {pokemon.types.map((typeItem) => (
+            <span key={typeItem.type.name} className={`pokemon-type ${typeItem.type.name}`}>
+              {typeItem.type.name}
+            </span>))}
+        </div>
+        {/*Botón para cambiar la imagen del Pokémon a Shiny */}
+        <button className = "isShiny" color = "Error" type = "button" onClick={() => setIsShiny((current) => !current)}> 
+        shiny
+        </button>
+    
 
 
 
      
 
-    </article>
+      </article>
+    </div>
 
 
     
