30a3fa92e82bbafe61f8615306e36b4fa48af91d
David Sánchez <164054263+Djave17@users.noreply.github.com>
2026-09-16 10:32:46 -0600

    Pokemon cards - Shiny button

--- pokemon-app/src/features/pokemon/components/PokemonCard.tsx
+++ pokemon-app/src/features/pokemon/components/PokemonCard.tsx
@@ -1,63 +1,68 @@
 import { useState } from "react";
-import type {  PokemonResponse } from "../types/pokemon";
+import type { PokemonResponse } from "../types/pokemon";
+import { Button, FormControlLabel, Switch } from "@mui/material"
 import "./PokemonStyle.css"
+import { Form } from "react-router";
 
 
 //Props: objeto que contiene la información del Pokémon
-interface PokemonCardProps {    
+interface PokemonCardProps {
   pokemon: PokemonResponse;
 }
 
 export function PokemonCard({ pokemon }: PokemonCardProps) {
 
-  const [isShiny, setIsShiny] = useState(false); 
+  const [isShiny, setIsShiny] = useState(false);
   const artwork = pokemon.sprites.other["official-artwork"]
   const imageUrl = isShiny ? artwork.front_shiny ?? artwork.front_default : artwork.front_default
   const id = pokemon.id.toString().padStart(3, "0")
   const height = pokemon.height / 10
-  const weight = pokemon.weight / 10 
-  
-  
-  
-  return ( 
-    <div className="bg-neutral-900 min-h-screen flex items-center justify-center p-4">
-      <article className="card">
-        <p>#{id}</p>
-        <h2> <b>{pokemon.name.toLocaleUpperCase()}</b> </h2>
-        {imageUrl ? ( <img className="pokemon-image" src={imageUrl} alt={pokemon.name} />) : ( <p>No hay imagen disponible</p> )}
+  const weight = pokemon.weight / 10
 
-        <div className = "pokemon-dimensions"> 
-          <div>
-            <strong>{weight} kg</strong>
-            <span> Weight</span> 
-          </div>
-          <div> 
-            <strong>{height} m</strong>
-            <span> Height</span>
-            
-          </div>
-          
+
+
+  return (
+
+    <article className="card">
+      <p>#{id}</p>
+      <h2> <b>{pokemon.name.toLocaleUpperCase()}</b> </h2>
+      {imageUrl ? (<img className="pokemon-image" src={imageUrl} alt={pokemon.name} />) : (<p>No hay imagen disponible</p>)}
+
+      <div className="pokemon-dimensions">
+        <div>
+          <strong>{weight} kg</strong>
+          <span> Weight</span>
         </div>
-        <div className = "pokemon-types"> 
-          {pokemon.types.map((typeItem) => (
-            <span key={typeItem.type.name} className={`pokemon-type ${typeItem.type.name}`}>
-              {typeItem.type.name}
-            </span>))}
+        <div>
+          <strong>{height} m</strong>
+          <span> Height</span>
+
         </div>
-        {/*Botón para cambiar la imagen del Pokémon a Shiny */}
-        <button className = "isShiny" onClick={() => setIsShiny((current) => !current)}> 
-        shiny
-        </button>
-    
+
+      </div>
+      <div className="pokemon-types">
+        {pokemon.types.map((typeItem) => (
+          <span key={typeItem.type.name} className={`pokemon-type ${typeItem.type.name}`}>
+            {typeItem.type.name}
+          </span>))}
+      </div>
+      {/*Botón para cambiar la imagen del Pokémon a Shiny */}
+
+      <FormControlLabel
+        control={
+          <Switch checked={isShiny} onChange={() => setIsShiny((current) => !current)} />}
+        label="Shiny"
+      />
 
 
 
-     
-
-      </article>
-    </div>
 
 
-    
+
+    </article>
+
+
+
+
   )
 } 
