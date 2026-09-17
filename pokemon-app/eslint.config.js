5f8fc1a730f64469ef34cf040fd6cceaa2ff9d93
David Sánchez <164054263+Djave17@users.noreply.github.com>
2026-09-17 08:02:08 -0600

    Fix: Components, isMobile

--- pokemon-app/eslint.config.js
+++ pokemon-app/eslint.config.js
@@ -19,4 +19,11 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
+  {
+    files: ['src/components/ui/**/*.tsx'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  },
+
 ])
